eb252e6e0a88b6de317774ca62c524948f2b6376
PrakashReddy17051997 <108370019+PrakashReddy17051997@users.noreply.github.com>
2026-04-03 21:16:17 +0200

    Feature 1 (#1) to main branch
    * feature-1 initial commit
    
    * } added
    
    * updated using shared library module
    
    * : unexpected token: component @ line 7, column 5.
           component: "catalogue"
    
    * 1.0.1 version update
    
    ---------
    
    Co-authored-by: orakanti <orakanti@rptu.de>

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use(bodyParser.json());
 
 app.get('/health', (req, res) => {
     var stat = {
-        app: 'OK',
+        app: 'OKay',
         mongo: mongoConnected
     };
     res.json(stat);
